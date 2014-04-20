e113ae2d86fcf3e45922783c8265fa7fe9ea0e20
masson.stephane@gmail.com <masson.stephane@gmail.com@ace3e766-64d9-c0f2-fe18-509798725781>
2014-04-20 12:18:19 +0000

    RainbowduinoV3 standalone text display

---
+++ trunk/trunk-arduino/src/RainbowduinoV3/drawCharPmRobotix/drawCharPmRobotix.ino
@@ -0,0 +1,30 @@
+/*
+ Print "text" value (one letter after one), with a random color.
+ based on "drawChar" example
+*/
+
+#include <Rainbowduino.h>
+
+char* text = "PM-ROBOTIX";
+
+void setup()
+{
+  Rb.init();
+}
+
+//unsigned char x,y,z;
+
+void loop()
+{
+  for(int i=0; i<strlen(text); i++)
+  {
+     Rb.drawChar(text[i],0,1,random(0xFFFFFF)); 
+     delay(500);
+     Rb.blankDisplay();
+  }
+  
+}
+
+
+
+
