720e64272cbd8ec7b01fc1228e3ee0ae101efdb2
masson.stephane@gmail.com <masson.stephane@gmail.com@ace3e766-64d9-c0f2-fe18-509798725781>
2014-04-20 18:00:02 +0000

    Update text by I2C

--- trunk/trunk-arduino/src/RainbowduinoV3/drawCharPmRobotix/drawCharPmRobotix.ino
+++ trunk/trunk-arduino/src/RainbowduinoV3/drawCharPmRobotix/drawCharPmRobotix.ino
@@ -1,30 +1,43 @@
 /*
  Print "text" value (one letter after one), with a random color.
  based on "drawChar" example
+ 
+ text can be updated by i2c (see i2cMaster)
 */
 
 #include <Rainbowduino.h>
+#include <Wire.h>
 
-char* text = "PM-ROBOTIX";
+char* text = "PM-ROBOTIX                                    ";
+int textSize = 11;
 
 void setup()
 {
   Rb.init();
+  
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
 }
 
-//unsigned char x,y,z;
 
 void loop()
 {
-  for(int i=0; i<strlen(text); i++)
+  for(int i=0; i<textSize; i++)
   {
      Rb.drawChar(text[i],0,1,random(0xFFFFFF)); 
      delay(500);
      Rb.blankDisplay();
-  }
-  
+  } 
 }
 
 
-
-
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  textSize = howMany;
+  for(int i=0; i<textSize; i++)
+  {
+    text[i] =  Wire.read();
+  }
+}
---
+++ trunk/trunk-arduino/src/RainbowduinoV3/i2cMaster/i2cMaster.ino
@@ -0,0 +1,31 @@
+/**
+ * Test I2C master (arduino) used to send text to a salve rainboduino  
+ */
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin(); // join i2c bus (address optional for master)
+}
+
+void loop()
+{
+  sendText(4, "C EST GAGNE  ");
+  delay(30000);
+
+  sendText(4, "OUI DIDITE!  ");
+  delay(30000);
+
+  // no text
+  sendText(4, " ");
+  delay(10000);
+  
+}
+
+void sendText(int salveAdress, char* text)
+{
+  Wire.beginTransmission(salveAdress);
+  Wire.write(text);
+  Wire.endTransmission();    // stop transmitting
+}
